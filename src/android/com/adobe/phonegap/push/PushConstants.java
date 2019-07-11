44089f3e13ec65c4f688329579e17b8f75a14a5d
michael92 <michalsiwek0529@gmail.com>
2019-07-11 23:45:18 +0200

    added new const for dismissed notifications

--- src/android/com/adobe/phonegap/push/PushConstants.java
+++ src/android/com/adobe/phonegap/push/PushConstants.java
@@ -85,7 +85,7 @@ public interface PushConstants {
     public static final String SUBJECT = "subject";
     public static final String GOOGLE_APP_ID = "google_app_id";
     public static final String GCM_DEFAULT_SENDER_ID = "gcm_defaultSenderId";
-
+    public static final String PUSH_DISMISSED = "push_dismissed";
     public static final String DEFAULT_CHANNEL_ID = "PushPluginChannel";
     public static final String CHANNEL_ID = "id";
     public static final String CHANNEL_DESCRIPTION = "description";
